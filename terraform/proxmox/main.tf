dd596f522d65f19ec7baa4699af03ea29d390341
ashutosh.kanojia.ak@gmail.com <ashutosh.kanojia.ak@gmail.com>
2025-08-25 20:01:33 +0530

    update main single node provision

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,73 +1,78 @@
-resource "proxmox_vm_qemu" "test" {
-  name = "tf-test"
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc03"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://xxx.xxx.xxx.xxx:8006/api2/json"
+  pm_api_token_id     = "XXXX"
+  pm_api_token_secret = "XXXX"
+  pm_tls_insecure     = true
+}
+
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+  name = "terraform-test"
 
   target_node = "pxmx02"
 
-  #clone from
-  clone_id   = 1100
-  full_clone = true
+  clone = "ubuntu-2404-base-template"
+
+  # Activate QEMU agent for this VM
+  agent = 1
+  memory  = 2048
+  scsihw  = "virtio-scsi-single"
+
+  # CloudInit configurations
+  ciuser = "ashu"
+  cipassword = "ashu"
+  sshkeys = "XXXX"
+  ipconfig0="ip=dhcp"
   
-
-  #cloud-init options
-  memory  = 1024
-  os_type = "cloud_init"
-  cores   = 2
-  sockets = 1
-#   agent = 1
-
-  # Disk parameters
-  scsihw = "virtio-scsi-single"
+  cpu {
+    cores = 1
+    sockets =1
+  }
 
   disks {
     ide {
       ide2 {
         cloudinit {
           storage = "local"
-        #   storage = "local-lvm" # deleted the lvm in proxmox and using local
         }
       }
     }
-
+    
     scsi {
       scsi0 {
+        # match the size with the template size or larger
         disk {
+          size    = 30
+          cache   = "writeback"
           storage = "local"
-        #   storage = "local-lvm" # deleted the local-lvm in proxmox and using local
-          size    = "20G"
-          # discard=true
-          cache     = "writeback"
-          replicate = true
         }
       }
     }
   }
 
-  network {
-    model  = "virtio"
+  network{
+    id=0
+    model = "virtio"
     bridge = "vmbr0"
-    id     = 0
   }
 
-  boot      = "order=scsi0"
-  ipconfig0 = "ip=dhcp"
-
-  serial {
-    id   = 0
-    type = "socket"
+  boot = "order=scsi0"
+  
+  serial{
+    id=0
+    type="socket"
   }
-
-#   ciuser     = "xxxx"
-#   cipassword = "xxxxxxxx"
-#   sshkeys = <<EOF
-#     ssh-rsa xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx user@pc
-#     EOF
-
-
-  lifecycle {
-    ignore_changes = [ network ]
-  }
-
 
 }
+
 
 
