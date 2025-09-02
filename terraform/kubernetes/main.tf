edd9990b991a2e02f2844f6cf6370ea8bb5c34f1
ashutosh.kanojia.ak@gmail.com <ashutosh.kanojia.ak@gmail.com>
2025-09-02 16:25:09 +0530

    k8s lab test

---
+++ terraform/kubernetes/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc03"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://${var.pm_host}:8006/api2/json"
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = true
+  alias               = "module"
+}
+
+variable "pm_host" {
+  description = "Proxmox cluster/node host. IP or FQDN"
+  type        = string
+}
+
+variable "pm_api_token_id" {
+  description = "Proxmox Token ID"
+  type        = string
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox Token secret"
+  type        = string
+}
+
+variable "ci_ssh_key" {
+  description = "CloudInit SSH keys for the host"
+  type        = string
+}
+
+variable "vm_cpu" {
+  type = object({
+    cores = number
+    # limit   = number
+    # numa    = false
+    # sockets = 1
+    type = optional(string, "host")
+    # units   = 0
+    # vcores  = 0
+  })
+  default = {
+    cores = 1
+  }
+}
+
+locals {
+  configs = yamldecode(file("${path.module}/kube-lab.yaml"))
+}
+
+module "k8s-control-plane" {
+  for_each = local.configs
+  source   = "../proxmox_module"
+  providers = {
+    proxmox = proxmox.module
+  }
+
+  # from kube-lab config
+  vm_name          = each.value.vm_name
+  vm_template_name = each.value.vm_template_name
+  pm_host          = each.value.pm_host
+  vm_cpu           = try(each.value.cpu, var.vm_cpu)
+  vm_memory        = each.value.memory
+
+  # from env variable
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  ci_ssh_key          = var.ci_ssh_key
+
+
+}
